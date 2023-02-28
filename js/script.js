d947cd5ca15114b2de8d4ffc9319b11150c71ef7
javierrm11 <javierrumo2@gmail.com>
2023-02-28 01:39:19 +0100

    biosixblue

---
+++ js/script.js
@@ -0,0 +1,25 @@
+window.addEventListener("scroll",function(){
+    var header=document.querySelector("h5.sabiasque");
+    header.classList.toggle("abajo",window.scrollY>800);
+})
+
+window.addEventListener("scroll",function(){
+    var header=document.querySelector("div.apartado11");
+    header.classList.toggle("abajo",window.scrollY>800);
+})
+
+window.addEventListener("scroll",function(){
+    var header=document.querySelector("div.apartado22");
+    header.classList.toggle("abajo",window.scrollY>800);
+})
+
+window.addEventListener("scroll",function(){
+    var header=document.querySelector("div.apartado33");
+    header.classList.toggle("abajo",window.scrollY>800);
+})
+
+window.addEventListener("scroll",function(){
+    var header=document.querySelector("div.material1");
+    header.classList.toggle("abajo",window.scrollY>800);
+})
+
